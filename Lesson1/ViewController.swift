ac44fa371e5a605cb23e6d30c0f91e3d33bf380f
Sergey <piolersa85@mail.ru>
2020-10-15 01:23:56 +0200

    Initial Commit

---
+++ Lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by Sergey Kalinin on 15.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
